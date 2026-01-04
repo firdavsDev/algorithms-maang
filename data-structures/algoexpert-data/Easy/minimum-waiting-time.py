ba1e31c027f62bc3c753b7bd29f056f3888fc690
firdavsDev <xackercoder@gmail.com>
2026-01-04 19:30:21 +0500

    Refactor code for improved readability and consistency across multiple algorithms
    - Standardized indentation and spacing in binary search, bubble sort, and other algorithms.
    - Enhanced readability by adding spaces around operators and after commas.
    - Removed unnecessary comments and improved existing ones for clarity.
    - Ensured consistent function definitions and return statements.
    - Cleaned up code in various algorithms including depth-first search, caesar cipher encryptor, and more.

--- data-structures/algoexpert-data/Easy/minimum-waiting-time.py
+++ data-structures/algoexpert-data/Easy/minimum-waiting-time.py
@@ -1,16 +1,15 @@
-
 # MINIMUM WAITING TIME
+
 
 # O(NlogN) time and O(1) space
 def minimumWaitingTime(queries):
     # Write your code here.
     queries.sort()
-	i = 0
-	totalTime = 0
-	runningSum = 0
-	while i < len(queries):
-		totalTime += runningSum
-		runningSum += queries[i]
-		i += 1
-	return totalTime
-
+    i = 0
+    totalTime = 0
+    runningSum = 0
+    while i < len(queries):
+        totalTime += runningSum
+        runningSum += queries[i]
+        i += 1
+    return totalTime
--- data-structures/algoexpert-data/Easy/node-depths.py
+++ data-structures/algoexpert-data/Easy/node-depths.py
@@ -1,16 +1,21 @@
-
 # NODE DEPTHS
+
 
 # SOLUTION 1
 def nodeDepths(root):
     # Write your code here.
     return calculateNodeDepths(root, 0)
 
+
 def calculateNodeDepths(root, currentDepth):
-	if not root:
-		return 0
-	
-	return currentDepth + calculateNodeDepths(root.left, currentDepth + 1) + calculateNodeDepths(root.right, currentDepth + 1)
+    if not root:
+        return 0
+
+    return (
+        currentDepth
+        + calculateNodeDepths(root.left, currentDepth + 1)
+        + calculateNodeDepths(root.right, currentDepth + 1)
+    )
 
 
 # This is the class of the input binary tree.
@@ -19,28 +24,29 @@ class BinaryTree:
         self.value = value
         self.left = None
         self.right = None
+
 
 # SOLUTION 2
 def nodeDepths(root):
     # Write your code here.
-	if not root:
-		return 0
-	totalDepth = 0
-	currentDepth = 0
+    if not root:
+        return 0
+    totalDepth = 0
+    currentDepth = 0
     levelOrderQueue = [root]
-	while levelOrderQueue:
-		totalDepth += len(levelOrderQueue) * currentDepth
-		currentDepth += 1
-		tempQueue = []
-		while levelOrderQueue:
-			tempNode = levelOrderQueue.pop()
-			if tempNode.left:
-				tempQueue.append(tempNode.left)
-			if tempNode.right:
-				tempQueue.append(tempNode.right)
-		levelOrderQueue = tempQueue
-		
-	return totalDepth
+    while levelOrderQueue:
+        totalDepth += len(levelOrderQueue) * currentDepth
+        currentDepth += 1
+        tempQueue = []
+        while levelOrderQueue:
+            tempNode = levelOrderQueue.pop()
+            if tempNode.left:
+                tempQueue.append(tempNode.left)
+            if tempNode.right:
+                tempQueue.append(tempNode.right)
+        levelOrderQueue = tempQueue
+
+    return totalDepth
 
 
 # This is the class of the input binary tree.
@@ -50,20 +56,21 @@ class BinaryTree:
         self.left = None
         self.right = None
 
-#SOLUTION 3
+
+# SOLUTION 3
 def nodeDepths(root):
     # Write your code here.
     stack = [{"node": root, "depth": 0}]
-	totalDepth = 0
-	while len(stack) > 0:
-		print(stack)
-		tempNode = stack.pop()
-		if not tempNode["node"]:
-			continue
-		totalDepth += tempNode["depth"]
-		stack.append({"node": tempNode["node"].left, "depth": tempNode["depth"] + 1})
-		stack.append({"node": tempNode["node"].right, "depth": tempNode["depth"] + 1})
-	return totalDepth
+    totalDepth = 0
+    while len(stack) > 0:
+        print(stack)
+        tempNode = stack.pop()
+        if not tempNode["node"]:
+            continue
+        totalDepth += tempNode["depth"]
+        stack.append({"node": tempNode["node"].left, "depth": tempNode["depth"] + 1})
+        stack.append({"node": tempNode["node"].right, "depth": tempNode["depth"] + 1})
+    return totalDepth
 
 
 # This is the class of the input binary tree.
